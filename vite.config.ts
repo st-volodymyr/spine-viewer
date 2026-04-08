a13f49d3b0775fd20d6262e619b29479c3c93cc6
VStymkovskyi <volodymyr.stymkovskyi@onseo.biz>
2026-04-08 08:26:23 +0300

    spine viewer implementation, remove hash from files in the build

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,13 @@ export default defineConfig({
     build: {
         outDir: 'docs',
         sourcemap: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name].[ext]',
+            },
+        },
     },
     server: {
         host: '0.0.0.0', // accessible on LAN for QA team
